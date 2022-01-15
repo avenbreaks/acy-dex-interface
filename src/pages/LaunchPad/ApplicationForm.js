04a2bcd394c2f4e3f570684bf822c9ab091c3732
AusintJagua <46783484+austin0072009@users.noreply.github.com>
2022-01-15 16:33:09 +0800

    wip: Acy Application form interface framework done

--- src/pages/LaunchPad/ApplicationForm.js
+++ src/pages/LaunchPad/ApplicationForm.js
@@ -1,5 +1,7 @@
 import { useEffect } from "react"
 import './css/Form.css';
+import { Form, Input, Button, Checkbox } from 'antd';
+
 
 
 const ApplicationForm = ()=> {
@@ -20,8 +22,7 @@ const ApplicationForm = ()=> {
             </header>
             <div className="row form-content">
                 <div className="col col-md-10 mr-md-auto ml-md-auto">
-                  <form action="" id="quoteForm" method="post">
-                   <ul>         
+                  <Form action="" id="quoteForm" method="post">
                                 <li className="form-group bmd-form-group">
                                     <label for="brand" className=" bmd-label">Office Email Address:</label>
                                     <input type="text" id="Email" name="Email"/>
@@ -43,8 +44,17 @@ const ApplicationForm = ()=> {
                                     <input type="text" id="TokenSymbol" name="TokenSymbol"/>
                                 </li>
                                 <li className="form-group bmd-form-group">
-                                    <label for="brand" className=" bmd-label">Token Contract Address(Chain Included):</label>
+                                    <label for="brand" className=" bmd-label">Token Contract Address:</label>
                                     <input type="text" id="TokenAddr" name="TokenAddr"/>
+                                </li>
+                                <li className="form-group bmd-form-group">
+                                    <label for="brand" className=" bmd-label">Token Network (Chain):</label>
+                                    <select class ="form-control" id="model">
+                                    <option value="">- Select -</option>
+                                    <option value="137">Polygon 137</option>
+                                    <option value="56">BSC(Binance Smart Chain) 56</option>  
+
+                                    </select>
                                 </li>
                                 <li className="form-group bmd-form-group">
                                     <label for="brand" className=" bmd-label">Project Description:</label>
@@ -82,9 +92,71 @@ const ApplicationForm = ()=> {
                                     <label for="brand" className=" bmd-label">Token Economics Link:</label>
                                     <input type="text" id="Email" name="Email"/>
                                 </li>
-        
+                                <li className="form-group bmd-form-group">
+                                    <label for="brand" className=" bmd-label">Your Project Build on:</label>
+                                    <select class ="form-control" id="model">
+                                    <option value="">- Select -</option>
+                                    <option value="1">Ethereum(ETH)</option>
+                                    <option value="2">Polygon (MATIC)</option>  
+                                    <option value="3">Avalanche (AVAX)</option>  
+                                    <option value="4">Fantom (FTM)</option>  
+                                    <option value="5">Conflux (CFX)</option>  
+                                    <option value="6">Arbitrum</option>  
+                                    <option value="7">Optimism</option>  
+                                    <option value="8">Solana</option>  
+                                    <option value="9">Near</option>  
+                                    <option value="10">Harmony</option>  
+                                    <option value="11">other</option>  
+                                    </select>
+                                </li>
+                                <li className="form-group bmd-form-group">
+                                    <label for="brand" className=" bmd-label">Industry Project Category:</label>
+                                    <select class ="form-control" id="model">
+                                    <option value="">- Select -</option>
+                                    <option value="1">Defi</option>
+                                    <option value="2">NFT</option>  
+                                    <option value="3">GameFi</option>  
+                                    <option value="4">SocialFi</option>  
+                                    <option value="5">Metaverse</option>  
+                                    <option value="6">Blockchain</option>  
+                                    <option value="7">Meme</option>  
+                                    <option value="8">Stablecoin</option>  
+                                    <option value="9">Storage</option>  
+                                    <option value="10">other</option>  
+                                    </select>
+                                </li>              
+                                
+                                <li className="form-group bmd-form-group">
+                                    <label for="brand" className=" bmd-label">WhitePaper Link:</label>
+                                    <input type="text" id="Email" name="Email"/>
+                                </li>
 
-                    </ul>  
+                                <li className="form-group bmd-form-group">
+                                    <label for="brand" className=" bmd-label">Linkedin Link:</label>
+                                    <input type="text" id="Email" name="Email"/>
+                                </li>
+
+                                <li className="form-group bmd-form-group">
+                                    <label for="brand" className=" bmd-label">Telegram Community Link:</label>
+                                    <input type="text" id="Email" name="Email"/>
+                                </li>
+
+                                <li className="form-group bmd-form-group">
+                                    <label for="brand" className=" bmd-label">Github Link:</label>
+                                    <input type="text" id="Email" name="Email"/>
+                                </li>
+
+                                <li className="form-group bmd-form-group">
+                                    <label for="brand" className=" bmd-label">Twitter Link:</label>
+                                    <input type="text" id="Email" name="Email"/>
+                                </li>
+
+                                
+                                <li className="form-group bmd-form-group">
+                                    <label for="brand" className=" bmd-label">Discord Link:</label>
+                                    <input type="text" id="Email" name="Email"/>
+                                </li>
+        
                           <div id="loading">
                               <div className="spinner">
                                   <div className="bounce1"></div>
@@ -97,7 +169,7 @@ const ApplicationForm = ()=> {
                                   <button type="submit" className="btn btn-raised btn-primary">APPLY 
                                   </button>
                           </div>
-                  </form> 
+                  </Form> 
                 </div>
           </div>
       </div> 

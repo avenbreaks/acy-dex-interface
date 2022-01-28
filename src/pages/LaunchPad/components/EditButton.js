32e5dd926594c9253ad4457c5ca8b50c712b5c40
zengzengzenghuy <tayzh2799@gmail.com>
2022-01-28 23:41:44 +0800

    wip: LaunchPad Pending Edit Button

---
+++ src/pages/LaunchPad/components/EditButton.js
@@ -0,0 +1,11 @@
+import React from 'react'
+// import "../css/ProjectsCard.css"
+// import telegramWIcon from '@/assets/icon_telegram_white.svg';
+
+const EditButton = () => {
+    return (
+        <div style={{ fontSize: 20, color: "white", fontWeight: "bold", alignSelf: "center" }}>Edit</div>
+    )
+}
+
+export default EditButton
